03c0780fc04aad0f9b73b488f9b8db555a2ab5ae
akrawczyk <agammkrawczyk@gmail.com>
2020-05-01 21:46:31 +0200

    add basic  components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { SiderbarComponent } from './components/siderbar/siderbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    SiderbarComponent
   ],
   imports: [
     BrowserModule
