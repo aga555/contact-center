ac8ffe11de47c6a2e4743256a8e405edb36596a8
akrawczyk <agammkrawczyk@gmail.com>
2021-08-08 20:53:06 +0200

    refactor remove header and footer, addd sidebar to fixed postion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,11 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
-import {HeaderComponent} from './components/header/header.component';
 import {HomeComponent} from './components/home/home.component';
 import {SiderbarComponent} from './components/siderbar/siderbar.component';
 import {ContactDetailsComponent} from './components/contatct-details/contact-details.component';
 import {FullnamePipe} from './pipes/fullname.pipe';
 import {HttpClientModule} from '@angular/common/http';
-import {FooterComponent} from './components/footer/footer.component';
 import {ListContactsComponent} from './components/list-contacts/list-contacts.component';
 import {AppRoutingModule} from './app-routing.module';
 import {AddContactComponent} from './components/add-contact/add-contact.component';
@@ -18,12 +16,10 @@ import {EditContactComponent} from './components/edit-contact/edit-contact.compo
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     HomeComponent,
     SiderbarComponent,
     ContactDetailsComponent,
     FullnamePipe,
-    FooterComponent,
     ListContactsComponent,
     AddContactComponent,
     EditContactComponent,
